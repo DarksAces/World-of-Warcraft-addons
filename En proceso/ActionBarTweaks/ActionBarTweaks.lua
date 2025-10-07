c91a9a9186651b0d7fcb3ee15873f6a59ebbe88d
Daniel <166744714+DarksAces@users.noreply.github.com>
2025-10-07 16:07:51 +0200

    Update ActionBarTweaks.lua

--- En proceso/ActionBarTweaks/ActionBarTweaks.lua
+++ En proceso/ActionBarTweaks/ActionBarTweaks.lua
@@ -1,20 +1,24 @@
 local f = CreateFrame("Frame")
 f:RegisterEvent("PLAYER_LOGIN")
 f:SetScript("OnEvent", function()
-    -- Esperar a que todo cargue
-    local bar = MainMenuBar
-
-    if bar then
-        -- Mover la barra más arriba
-        bar:ClearAllPoints()
-        bar:SetPoint("BOTTOM", UIParent, "BOTTOM", 0, 120)
-
-        -- Cambiar escala (más pequeña)
-        bar:SetScale(0.9)
-
-        -- Mensaje de confirmación
-        print("|cff00ff00[ActionBarTweaks]|r Barra movida correctamente.")
-    else
-        print("|cffff0000No se pudo encontrar la barra principal.|r")
-    end
+    C_Timer.After(1, function() -- Esperar un segundo para que todo cargue
+        local bar = MainMenuBarArtFrame
+        
+        if bar then
+            bar:ClearAllPoints()
+            bar:SetPoint("BOTTOM", UIParent, "BOTTOM", 0, 120)
+            bar:SetScale(0.9)
+            
+            -- También mover las barras de acción
+            if MainMenuBar then
+                MainMenuBar:ClearAllPoints()
+                MainMenuBar:SetPoint("BOTTOM", UIParent, "BOTTOM", 0, 120)
+                MainMenuBar:SetScale(0.9)
+            end
+            
+            print("|cff00ff00[ActionBarTweaks]|r Barra movida correctamente.")
+        else
+            print("|cffff0000No se pudo encontrar la barra principal.|r")
+        end
+    end)
 end)
